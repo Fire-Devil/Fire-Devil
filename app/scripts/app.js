24d09d80ad92503965dbe9afd311b017854da315
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-01-29 10:15:36 -0800

    Creating a userSwipe object with UTC of swipes.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,29 +12,47 @@ var fireDevil = angular.module('fireDevilApp', ['firebase'])
     var ref = new Firebase('https://fire-devil.firebaseio.com/');
     var sync = $firebase(ref);
     var record = sync.$asObject();
+    var userSwipes = {};
 
-    // Using promises:
-    record.$loaded().then(function() {
-      for (var key in record) {
-        if (key[0] !== '$' && key !== 'forEach') {
-          originalUserStorage[key] = record[key];
+    // Helper function for counting swipes.
+    var countStrokes = function(userObj, userName) {
+      // Create empty object for each user in userSwipes.
+      if (userSwipes[userName] === undefined) {
+        userSwipes[userName] = {
+          utc: undefined,
+          mood: {
+            happy: 0,
+            excited: 0,
+            sad: 0,
+            angry: 0
+          }
         }
       }
-      $scope.originalUserStorage = originalUserStorage;
-      console.log(originalUserStorage);
-    });
-    // record.$loaded().then(function() {
-    //   console.log("record:", record);
-    // });
+      for (var key in userObj) {
+        // console.log('userObj[key]', userObj[key]);
+        if (userSwipes['utc'] === undefined) {
+          if (typeof userObj[key][2] === 'number') {
+            userSwipes[userName]['utc'] = userObj[key][2];
+          }
+        }
+      }
+    };
 
-    // ref.on('value',
-    //   function(snapshot) {
-    //     originalUserStorage = snapshot.val();
-    //     console.log('originalUserStorage', originalUserStorage);
-    //   }, function(error) {
-    //   console.log('error', error);
-    // });
-
-    // console.log('originalUserStorage', originalUserStorage);
-    // $scope.originalUserStorage = originalUserStorage;
-  }]);
+    // Using promises to get user data:
+    record.$loaded().
+      then(function() {
+        for (var key in record) {
+          if (key[0] !== '$' && key !== 'forEach') {
+            originalUserStorage[key] = record[key];
+          }
+        }
+        $scope.originalUserStorage = originalUserStorage;
+      })
+      // Using promises to prepare the userSwipes object for population.
+      .then(function() {
+        for (var key in originalUserStorage) {
+          countStrokes(originalUserStorage[key], key);
+        }
+          console.log('userSwipes', userSwipes);
+      });
+}]);
