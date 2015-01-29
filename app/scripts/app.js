065e7f76856c8ddc5c4187e962d80500ef350328
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-01-28 17:52:17 -0800

    Working on adding d3 visualization to index.html via Angular.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * @ngdoc overview
  * @name fireDevilApp
@@ -8,5 +6,35 @@
  *
  * Main module of the application.
  */
-angular
-  .module('fireDevilApp', ['firebase']);
+var fireDevil = angular.module('fireDevilApp', ['firebase'])
+  .controller('UserDataCtrl', ['$scope', '$firebase', function($scope, $firebase) {
+    var originalUserStorage = {};
+    var ref = new Firebase('https://fire-devil.firebaseio.com/');
+    var sync = $firebase(ref);
+    var record = sync.$asObject();
+
+    // Using promises:
+    record.$loaded().then(function() {
+      for (var key in record) {
+        if (key[0] !== '$' && key !== 'forEach') {
+          originalUserStorage[key] = record[key];
+        }
+      }
+      $scope.originalUserStorage = originalUserStorage;
+      console.log(originalUserStorage);
+    });
+    // record.$loaded().then(function() {
+    //   console.log("record:", record);
+    // });
+
+    // ref.on('value',
+    //   function(snapshot) {
+    //     originalUserStorage = snapshot.val();
+    //     console.log('originalUserStorage', originalUserStorage);
+    //   }, function(error) {
+    //   console.log('error', error);
+    // });
+
+    // console.log('originalUserStorage', originalUserStorage);
+    // $scope.originalUserStorage = originalUserStorage;
+  }]);
