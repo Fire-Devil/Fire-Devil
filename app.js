60208328775d45b20d7dd64e391030a845ec0ba1
Nick S <nsipplswezey@gmail.com>
2015-02-01 12:23:03 -0800

    Adds configuration for deployment

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = require('./server/server.js');
 
+app.set('port', (process.env.PORT || 1337));
 app.use('/',express.static(__dirname + '/client'));
 app.use(express.static(__dirname + '/client'));
-app.use('/data-view',express.static(__dirname +'/app'))
+app.use('/data-view',express.static(__dirname +'/app'));
 
 // app.get("/",function(req,/res){
 //    console.log('__dirname', __dirname);
@@ -11,4 +12,6 @@ app.use('/data-view',express.static(__dirname +'/app'))
 // })
 
 console.log('listening on 1337');
-app.listen(1337);
+app.listen(app.get('port'),function(){
+  console.log("Node app is running at localhost: " + app.get('port'));
+});
