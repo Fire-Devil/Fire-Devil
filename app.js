fe113a57e04ed3ffa43ae847be142c59a248afba
Nick S <nsipplswezey@gmail.com>
2015-02-01 11:53:49 -0800

    Sets deploy config. Adds npm-debug.log to .gitignore

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use('/',express.static(__dirname + '/client'));
 app.use(express.static(__dirname + '/client'));
+app.use('/data-view',express.static(__dirname +'/app'));
+
+
 //asks google for permissions of specific items defined in scopes
 app.get('/login',
   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile',
@@ -17,10 +20,11 @@ app.get('/login',
     // The request will be redirected to Google for authentication, so this
     // function will not be called.
   });
-app.use('/data-view',express.static(__dirname +'/app'));
+
 app.use('google-cal',express.static(__dirname + '/google-cal'));
+
 //callback defined in project, goes here to authenticate after route to login
-app.get('/data-view/callback', 
+app.get('/data-view/callback',
   passport.authenticate('google', { failureRedirect: '/data-view' }),
   function(req, res) {
     res.redirect('/data-view');
@@ -30,8 +34,10 @@ app.get('/data-view/callback',
     req.logout();
     res.redirect('/');
   });
+
+
+app.set('port', (process.env.PORT || 1337));
 console.log('listening on 8000');
-app.listen(8000);
 
 var ensureAuthenticated = function(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
